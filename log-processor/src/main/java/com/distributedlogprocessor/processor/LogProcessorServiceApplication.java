018745f74b5f4861673ecc166ebf21d6d95b916d
Dev-YassineCheracher <yassinecheracher@gmail.com>
2024-01-01 18:04:07 +0100

    init: Initial Project Setup

---
+++ log-processor/src/main/java/com/distributedlogprocessor/processor/LogProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distributedlogprocessor.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogProcessorServiceApplication.class, args);
+	}
+
+}
---
+++ log-retrieval/src/main/java/com/distributedlogprocessor/retrieval/LogRetrievalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.distributedlogprocessor.retrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRetrievalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogRetrievalServiceApplication.class, args);
+	}
+
+}
